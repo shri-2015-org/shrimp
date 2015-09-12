e1eaedc8cff9ba2ed1379f39c225d38d8751ed5e
Roman Fedorenkov <suffering123494@yandex.ru>
2015-09-12 19:51:53 +0300

    feat(server): added function to add new user in channel

--- server/src/core.js
+++ server/src/core.js
@@ -4,4 +4,8 @@ export function setChannels (state, channels) {
   return state.set('channels', channels);
 }
 
-export function addUserToChannel (state)
+export function addUserToChannel (state, channelId, userId) {
+  let index = state.get('channels').map((item) => { return item.get('id'); }).indexOf(channelId);
+
+  return state.updateIn(['channels', index, 'userIds'], 0, userIds => userIds.push(userId) );
+}
--- server/test/test_core.js
+++ server/test/test_core.js
@@ -1,17 +1,29 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
-import {setChannels} from '../src/core.js';
+import {setChannels, addUserToChannel} from '../src/core.js';
 
 describe('application logic', () => {
-    describe('setChannels', () => {
-        it('adds the channels to the state', () => {
-            const state = Map();
-            const channels = List.of(Map({id: 1, name: '1', userIds: List.of(1,2)}));
-            console.log(channels);
-            const nextState = setChannels(state, channels);
-            expect(nextState).to.equal(Map({
-                channels: List.of(Map({id: 1, name: '1', userIds: List.of(1,2)}))
-            }));
-        });
+  describe('setChannels', () => {
+    it('adds the channels to the state', () => {
+      const state = Map();
+      const channels = List.of(Map({id: 1, name: '1', userIds: List.of(1, 2)}));
+      const nextState = setChannels(state, channels);
+      expect(nextState).to.equal(Map({
+        channels: List.of(Map({id: 1, name: '1', userIds: List.of(1, 2)}))
+      }));
     });
+  });
+  describe('addUserToChannel', () => {
+    it('adds user\'s id to state\'s userIds', () => {
+      const state = Map({
+        channels: List.of(Map({id: 0, name: '1', userIds: List.of(1, 2)}))
+      });
+      const channelId = 0;
+      const userId = 3;
+      const nextState = addUserToChannel(state, channelId, userId);
+      expect(nextState).to.equal(Map({
+        channels: List.of(Map({id: 0, name: '1', userIds: List.of(1, 2, 3) }))
+      }));
+    });
+  });
 });
