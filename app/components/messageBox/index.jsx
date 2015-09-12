7e37db267354898943b98e6c4721f2c9e6a27776
Olga Likhacheva <likhacheva@internet-design.ru>
2015-09-12 18:49:24 +0300

    fix(components) text area enter

--- app/components/messageBox/index.jsx
+++ app/components/messageBox/index.jsx
@@ -13,47 +13,52 @@ export default class Index extends React.Component {
 	}
 	handleChange(event){
 		this.setState({
-			name: event.target.value
+			name: event.target.value.trim()
 		})
 	}
 	handleTextareaChange(event){
 		this.setState({
-			text: event.target.value
+			text: event.target.value.trim()
 		})
 	}
-	sendMessage(event){
+	sendMessage(){
 		//send to sever
 		//this.props.actions.sendMessage(this.state.text, this.state.name);
+		if (!this.state.text.trim() || !this.state.name.trim()) return;
 		this.setState({
 			name: "",
 			text: ""
 		})
 	}
+	handleKeyPress(e) {
+		if (e.which === 13 && !e.shiftKey){
+			this.sendMessage();
+			e.preventDefault();
+		}
+	}
 	render() {
 		return (
-			<form>
-				<div className="form-block">
-					<div className="form-block__form-item">
-						<div className="form-block__form-item__item-head">
-							<label>Username:</label>
-							<label>{this.state.name}</label>
-						</div>
-						<div className="form-block__form-item__item-body">
-							<input type="text" onChange={this.handleChange.bind(this)}/>
-						</div>
+			<div className="form-block">
+				<div className="form-block__form-item">
+					<div className="form-block__form-item__item-head">
+						<label>Username:</label>
+						<label>{this.state.name}</label>
 					</div>
-					<div className="form-block__form-item">
-						<div className="form-block__form-item__item-body">
-							<textarea onChange={this.handleTextareaChange.bind(this)}></textarea>
-						</div>
-					</div>
-					<div className="form-block__form-item">
-						<div className="form-block__form-item__item-body">
-							<button type="submit" onClick={this.sendMessage.bind(this)}>Send</button>
-						</div>
+					<div className="form-block__form-item__item-body">
+						<input type="text" value={this.state.name} onChange={this.handleChange.bind(this)}/>
 					</div>
 				</div>
-			</form>
+				<div className="form-block__form-item">
+					<div className="form-block__form-item__item-body">
+						<textarea value={this.state.text} onKeyPress={this.handleKeyPress.bind(this)} onChange={this.handleTextareaChange.bind(this)}></textarea>
+					</div>
+				</div>
+				<div className="form-block__form-item">
+					<div className="form-block__form-item__item-body">
+						<button type="submit" onClick={this.sendMessage.bind(this)}>Send</button>
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
