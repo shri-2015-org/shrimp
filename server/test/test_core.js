4baead8eb7c326f0a426ddb4d11dd635c19bccdf
Roman Fedorenkov <suffering123494@yandex.ru>
2015-09-13 12:26:25 +0300

    feat(server): finished main core functions to work with state

--- server/test/test_core.js
+++ server/test/test_core.js
@@ -1,6 +1,6 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
-import {setChannels, addUserToChannel} from '../src/core.js';
+import {setChannels, addChannel, removeChannel, addUserToChannel, removeUserFromChannel} from '../src/core.js';
 
 describe('application logic', () => {
   describe('setChannels', () => {
@@ -14,18 +14,66 @@ describe('application logic', () => {
     });
   });
 
+  describe('addChannel', () => {
+    it('add new channel', () => {
+      const state = Map({
+        channels: List.of(Map({id: 0, name: '0', userIds: List.of(1, 2)}))
+      });
+      const channel = Map({id: 1, name: '1', userIds: List.of(1, 2, 3) });
+      const nextState = addChannel(state, channel);
+      expect(nextState).to.equal(Map({
+        channels: List.of(
+          Map({id: 0, name: '0', userIds: List.of(1, 2) }),
+          Map({id: 1, name: '1', userIds: List.of(1, 2, 3) }),
+        )
+      }));
+    });
+  });
+
+  describe('removeChannel', () => {
+    it('remove channel by id', () => {
+      const state = Map({
+        channels: List.of(
+          Map({id: 0, name: '0', userIds: List.of(1, 2) }),
+          Map({id: 1, name: '1', userIds: List.of(1, 2, 3) }),
+        )
+      });
+      const channelId = 1;
+      const nextState = removeChannel(state, channelId);
+      expect(nextState).to.equal(Map({
+        channels: List.of(Map({id: 0, name: '0', userIds: List.of(1, 2) }))
+      }));
+    });
+  });
+
   describe('addUserToChannel', () => {
     it('adds user\'s id to state\'s userIds', () => {
       const state = Map({
-        channels: List.of(Map({id: 0, name: '1', userIds: List.of(1, 2)})),
+        channels: List.of(Map({id: 0, name: '0', userIds: List.of(1, 2)}))
       });
       const channelId = 0;
       const userId = 3;
       const nextState = addUserToChannel(state, channelId, userId);
       expect(nextState).to.equal(Map({
-        channels: List.of(Map({id: 0, name: '1', userIds: List.of(1, 2, 3) })),
+        channels: List.of(Map({id: 0, name: '0', userIds: List.of(1, 2, 3) }))
       }));
     });
   });
-});
 
+  describe('removeUserFromChannel', () => {
+    it('remove user\'s id from state\'s userIds', () => {
+      const state = Map({
+        channels: List.of(Map({id: 0, name: '0', userIds: List.of(1, 2, 3) }))
+      });
+      const channelId = 0
+      const userId = 2;
+      const nextState = removeUserFromChannel(state, channelId, userId);
+      console.log(nextState);
+      expect(nextState).to.equal(Map({
+        channels: List.of(Map({id: 0, name: '0', userIds: List.of(1, 3) }))
+      }))
+    });
+  });
+
+
+});
