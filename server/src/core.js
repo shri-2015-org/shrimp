3f1bea1e93f1d78c6303b2a33b18e0a00eb112bb
Максим Самойлов <samoilowmaxim@gmail.com>
2015-09-13 13:11:30 +0300

    style(server): resolve eslint errors

--- server/src/core.js
+++ server/src/core.js
@@ -1,10 +1,8 @@
-import {List, Map} from 'immutable';
-
-export function setChannels (state, channels) {
+export function setChannels(state, channels) {
   return state.set('channels', channels);
 }
 
-export function addUserToChannel (state, channelId, userId) {
+export function addUserToChannel(state, channelId, userId) {
   const index = state.get('channels').map(item => item.get('id')).indexOf(channelId);
 
   return state.updateIn(['channels', index, 'userIds'], 0, userIds => userIds.push(userId) );
