f5c8185e04404347bb65a6f3dd7775fce343ba78
Dmitri Pisarev <dimaip@gmail.com>
2015-09-14 14:37:49 +0300

    refactor appReducer: get rid of redundant switch
    For some reason just exporint result of `combineReducers` doesn't work:
    stores requires a function to be passed.

--- app/reducers/app_reducer.js
+++ app/reducers/app_reducer.js
@@ -2,9 +2,6 @@ import {combineReducers} from 'redux';
 import {channels} from './channels.js';
 import {messages} from './messages.js';
 
-export function appReducer(state, action) {
-  switch (action.type) {
-  default:
-    return combineReducers({channels, messages});
-  }
+export function appReducer() {
+  return combineReducers({channels, messages});
 }
