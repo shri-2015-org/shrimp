ed1e421cef5922ce2112532335a4dcb0df3a19ce
Olga Likhacheva <likhacheva@internet-design.ru>
2015-09-18 23:51:13 +0300

    ThreadsHeader draft: mutable state

--- app/components/Threads/index.jsx
+++ app/components/Threads/index.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import './styles.scss';
+import ThreadsHeader from 'components/ThreadsHeader';
 
+const peopleTab = { id: 0, name: 'People' };
+const channelsTab = { id: 1, name: 'Channels' };
+
+const tabs = [
+  peopleTab,
+  channelsTab,
+];
 
 export default class ThreadsSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTab: peopleTab.id,
+    };
+  }
+
+  changeTab = (tab) => {
+    this.setState({ currentTab: tab.id });
+  }
+
   render() {
     return (
-      <div className='threads'>threads</div>
+      <div className='threads'>
+        <ThreadsHeader tabs={tabs} changeTab={this.changeTab} currentTab={this.state.currentTab} />
+        {this.state.currentTab === peopleTab.id
+          ? <div>people will be here</div>
+          : null}
+        {this.state.currentTab === channelsTab.id
+          ? <div>channels will be here</div>
+          : null}
+      </div>
     );
   }
 }
