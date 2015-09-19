cc2a99d1e718a019ea93338a55cc61291788740c
Vlad Dubov <dubov.web@gmail.com>
2015-09-19 19:29:22 +0300

    Resolve eslint errors

--- server/models/chanel.js
+++ server/models/chanel.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const schema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: String,
-  userIds: [Schema.Types.ObjectId]
+  userIds: [Schema.Types.ObjectId],
 });
 
 export default function getChanelModel() {
