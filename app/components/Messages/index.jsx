0f94fc42e5d0bc86031661a578dc588671015222
faraser <faraser@rambler.ru>
2015-09-20 11:03:55 +0300

    feat(components): change paddingBottom when textarea height change

--- app/components/Messages/index.jsx
+++ app/components/Messages/index.jsx
@@ -11,17 +11,42 @@ export default class Messages extends React.Component {
     newMessage: PropTypes.func.isRequired,
   }
 
-  messagesScrollToBottom = () => {
+
+  componentDidMount = () => {
+    const list = this.refs.list.getDOMNode();
+    this.basePaddingBottom = parseInt(window.getComputedStyle(list).paddingBottom, 10);
+    this.baseTextareaHeight = null;
+  }
+
+
+  scrollToBottom = () => {
     const list = this.refs.list.getDOMNode();
     window.scrollTo(0, list.scrollHeight);
   }
+
+
+  changePaddingBottom = (height) => {
+    if (this.baseTextareaHeight === null) {
+      this.baseTextareaHeight = height;
+    }
+    const list = this.refs.list.getDOMNode();
+    list.style.paddingBottom = this.basePaddingBottom - this.baseTextareaHeight + height + 'px';
+    this.scrollToBottom();
+  }
+
 
   render() {
     const {messages, newMessage} = this.props;
     return (
       <div className='messages' ref='list'>
-        <MessageList messages={messages} scroll={this.messagesScrollToBottom} />
-        <MessageComposer newMessage={newMessage}/>
+        <MessageList
+          messages={messages}
+          scroll={this.scrollToBottom}
+          />
+        <MessageComposer
+          newMessage={newMessage}
+          changePaddingBottom={this.changePaddingBottom}
+          />
       </div>
     );
   }
