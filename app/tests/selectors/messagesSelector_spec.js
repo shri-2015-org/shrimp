85014698cb66c4dd5eb10d5679b6c6fbc7315900
Dmitri Pisarev <dimaip@gmail.com>
2015-09-21 21:04:58 +0300

    feat(selectors): fetch user name in `messagesSelector`
    Closes: #96

--- app/tests/selectors/messagesSelector_spec.js
+++ app/tests/selectors/messagesSelector_spec.js
@@ -7,19 +7,25 @@ describe('messages selector', () => {
     const currentChannelId = 1;
 
     const messages = List.of(
-      Map({id: 0, channelId: 0, text: 'first message'}),
-      Map({id: 1, channelId: 1, text: 'second message'}),
+      Map({id: 0, channelId: 0, senderId: 0, text: 'first message'}),
+      Map({id: 1, channelId: 1, senderId: 1, text: 'second message'}),
+    );
+
+    const users = List.of(
+      Map({id: 0, name: 'Vasya'}),
+      Map({id: 1, name: 'Petja'}),
     );
 
     const state = {
       messages,
+      users,
       currentChannelId,
     };
 
     const filteredState = currentChannelMessagesSelector(state);
 
     chai.expect(Map(filteredState)).to.equal(Map({
-      messages: List.of(Map({id: 1, channelId: 1, text: 'second message'})),
+      messages: List.of(Map({id: 1, channelId: 1, text: 'second message', senderId: 1, sender: Map({id: 1, name: 'Petja'})})),
       currentChannelId,
     }));
   });
