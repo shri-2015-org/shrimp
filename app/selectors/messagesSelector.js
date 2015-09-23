aa9cc1694c5a709527b04b288d9b0c37a4029759
Roman Fedorenkov <suffering123494@yandex.ru>
2015-09-23 10:31:52 +0300

    rebase with origin/master

--- app/selectors/messagesSelector.js
+++ app/selectors/messagesSelector.js
@@ -1,20 +1,14 @@
 import {createSelector} from 'reselect';
 
-<<<<<<< HEAD
 const messagesSelector = state => {
   return state.messages.map(message => {
     const senderObj = state.users.find(user => user.get('id') === message.get('senderId'));
     return message.set('sender', senderObj);
   });
 };
-
-const currentChannelIdSelector = (state) => state.currentChannelId;
-=======
-const messagesSelector = (state) => state.messages;
 const localSelector = (state) => state.local;
 const channelsSelector = (state) => state.channels;
 const usersSelector = (state) => state.users;
->>>>>>> feat(channel): filter messages by currentChannelId
 
 export const currentChannelMessagesSelector = createSelector(
   [messagesSelector, localSelector, channelsSelector, usersSelector],
