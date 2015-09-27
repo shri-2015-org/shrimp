1f30bacaa842824e4ee4f096257e78d3ea5deb46
Максим Самойлов <samoilowmaxim@gmail.com>
2015-09-27 19:52:51 +0300

    feat(login-page): add /login, /signup routing

--- app/components/LoginWindow/index.jsx
+++ app/components/LoginWindow/index.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import {pushState} from 'redux-router';
+import {connect} from 'react-redux';
 import {Spring} from 'react-motion';
 import Tabs from 'components/Tabs';
 import Tab from 'components/Tab';
 import './styles.scss';
 
 
+@connect(state => ({
+  location: state.router.location.pathname,
+}), {pushState})
 export default class LoginWindow extends React.Component {
+
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    location: PropTypes.string.isRequired,
+  }
 
   constructor(props) {
     super(props);
     this.state = {
-      currentTabId: 1,
+      currentTabId: (path => {
+        if (path === '/signup') {
+          return 2;
+        } else if (path === '/login') {
+          return 1;
+        }
+        return 1;
+      }(this.props.location)),
     };
   }
 
@@ -33,9 +50,10 @@ export default class LoginWindow extends React.Component {
           currentTabId={this.state.currentTabId}
           changeTab={this.changeTab}
         >
-          <Tab id={1} name='Log In' />
-          <Tab id={2} name='Sign Up' />
+          <Tab id={1} link='/login'>Log In</Tab>
+          <Tab id={2} link='/signup'>Sign Up</Tab>
         </Tabs>
+        {this.props.children}
       </div>
     );
 
---
+++ app/components/SignUp/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.scss';
+
+
+export default class SignUp extends React.Component {
+
+	render() {
+		return (
+			<div>SignUp</div>
+		);
+	}
+}
