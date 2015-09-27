619370cdabf426b929dc2fe6a4204f44a187772f
Максим Самойлов <samoilowmaxim@gmail.com>
2015-09-27 21:13:44 +0300

    feat(info-message): add info-message block

---
+++ app/components/InfoMessage/index.jsx
@@ -0,0 +1,32 @@
+import React, {PropTypes} from 'react';
+import cx from 'classnames';
+import './styles.scss';
+
+
+export default class InfoMessage extends React.Component {
+
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    className: PropTypes.string,
+    type: PropTypes.string,
+  }
+
+  render() {
+    const {type, className} = this.props;
+    const classes = cx(
+      [className],
+      'info-message',
+      {
+        'info-message_warning': type === 'warning',
+        'info-message_error': type === 'error',
+        'info-message_success': type === 'success',
+      }
+    );
+
+    return (
+      <div className={classes}>
+        <div className='info-message__text'>{this.props.children}</div>
+      </div>
+    );
+  }
+}
