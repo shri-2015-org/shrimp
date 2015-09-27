83df1f70063aa6fcb105cadde39bc6f8fe9aeea6
Максим Самойлов <samoilowmaxim@gmail.com>
2015-09-28 00:38:18 +0300

    feat(info-message): add animation for validation

--- app/components/InfoMessage/index.jsx
+++ app/components/InfoMessage/index.jsx
@@ -1,4 +1,5 @@
 import React, {PropTypes} from 'react';
+import {Spring} from 'react-motion';
 import cx from 'classnames';
 import './styles.scss';
 
@@ -9,23 +10,79 @@ export default class InfoMessage extends React.Component {
     children: PropTypes.node.isRequired,
     className: PropTypes.string,
     type: PropTypes.string,
+    shake: PropTypes.bool,
   }
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      shaking: false,
+      changing: false,
+      children: this.props.children,
+    };
+  }
+
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.children === this.props.children &&
+        nextProps.type === this.props.type &&
+        nextProps.shake === this.props.shake &&
+        nextState.changing === this.state.changing &&
+        nextState.children === this.state.children) {
+      return false;
+    }
+    if (nextProps.shake !== this.props.shake) {
+      this.setState({
+        shaking: this.props.shake,
+      });
+      return true;
+    }
+    if (!this.state.changing) {
+      this.setState({
+        changing: true,
+      });
+      setTimeout(() => {
+        this.setState({
+          changing: false,
+          children: nextProps.children,
+        });
+      }, 500);
+    }
+    return true;
+  }
+
 
   render() {
     const {type, className} = this.props;
     const classes = cx(
-      [className],
+      className,
       'info-message',
       {
         'info-message_type_warning': type === 'warning',
         'info-message_type_error': type === 'error',
         'info-message_type_success': type === 'success',
+        'info-message_shake': this.state.shaking,
       }
+    );
+
+    const getMessageText = (interpolated) => (
+      <div
+        className='info-message__text'
+        style={{transform: `translateX(-${interpolated.val}%)`}}
+      >
+        {this.state.children}
+      </div>
     );
 
     return (
       <div className={classes}>
-        <div className='info-message__text'>{this.props.children}</div>
+        <Spring
+          defaultValue={{val: 0}}
+          endValue={{val: this.state.changing ? 100 : 0, config: [1000, 100]}}
+        >
+          {(interpolated) => getMessageText(interpolated)}
+        </Spring>
       </div>
     );
   }
--- app/components/Login/index.jsx
+++ app/components/Login/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cx from 'classnames';
 import InfoMessage from 'components/InfoMessage';
 import PasswordInput from 'components/PasswordInput';
 import Input from 'components/Input';
@@ -9,8 +10,31 @@ import './styles.scss';
 
 export default class Login extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      info: {
+        type: 'info',
+        text: 'Enter your login and password',
+      },
+      shakePassword: false,
+    };
+  }
+
   auth = (e) => {
     e.preventDefault();
+    if (this.state.info.type === 'error' && this.state.info.code === 1001) {
+      this.setState({shakePassword: true});
+      setTimeout(() => this.setState({shakePassword: false}), 500);
+      return;
+    }
+    this.setState({
+      info: {
+        code: 1001,
+        type: 'error',
+        text: 'Wrong pass',
+      },
+    });
   }
 
 
@@ -19,7 +43,9 @@ export default class Login extends React.Component {
       <form className='login'>
         <InfoMessage
           className='login__info-message'
-        >Enter your login and password</InfoMessage>
+          type={this.state.info.type}
+          shake={this.state.shakePassword}
+        >{this.state.info.text}</InfoMessage>
         <Input className='login__input' placeholder='Login' />
         <PasswordInput className='login__input' />
         <Button
