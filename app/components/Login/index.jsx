6b2d1ef7606ed3b0a51dd4e48fbb6ac3a65d7402
Максим Самойлов <samoilowmaxim@gmail.com>
2015-09-27 22:14:24 +0300

    feat(login-page): add login and password inputs

--- app/components/Login/index.jsx
+++ app/components/Login/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import InfoMessage from 'components/InfoMessage';
+import Input from 'components/Input';
+import PasswordInput from 'components/PasswordInput';
 
 import './styles.scss';
 
@@ -8,10 +10,12 @@ export default class Login extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className='login'>
         <InfoMessage
           className='login__info-message'
         >Enter your login and password</InfoMessage>
+        <Input className='login__input' placeholder='login' />
+        <PasswordInput className='login__input' />
       </div>
     );
   }
