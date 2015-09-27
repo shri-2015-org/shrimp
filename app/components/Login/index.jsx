02d533e8f79473b297311056f8b0a7abd941fc4b
Максим Самойлов <samoilowmaxim@gmail.com>
2015-09-27 20:26:22 +0300

    style: replace tabs with two spaces

--- app/components/Login/index.jsx
+++ app/components/Login/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import InfoMessage from 'components/InfoMessage';
 
 import './styles.scss';
 
 
 export default class Login extends React.Component {
 
-	render() {
-		return (
-			<div>Login</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <InfoMessage status='info' text='some text' />
+      </div>
+    );
+  }
 }
--- app/components/SignUp/index.jsx
+++ app/components/SignUp/index.jsx
@@ -5,9 +5,9 @@ import './styles.scss';
 
 export default class SignUp extends React.Component {
 
-	render() {
-		return (
-			<div>SignUp</div>
-		);
-	}
+  render() {
+    return (
+      <div>SignUp</div>
+    );
+  }
 }
