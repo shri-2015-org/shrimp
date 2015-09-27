68100d9a34876ef11aca44b1ebd20a7967df714f
Максим Самойлов <samoilowmaxim@gmail.com>
2015-09-27 22:59:17 +0300

    feat(login-page): add submit button

--- app/components/Login/index.jsx
+++ app/components/Login/index.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import InfoMessage from 'components/InfoMessage';
-import Input from 'components/Input';
 import PasswordInput from 'components/PasswordInput';
+import Input from 'components/Input';
+import Button from 'components/Button';
 
 import './styles.scss';
 
 
 export default class Login extends React.Component {
 
+  auth = (e) => {
+    e.preventDefault();
+  }
+
+
   render() {
     return (
-      <div className='login'>
+      <form className='login'>
         <InfoMessage
           className='login__info-message'
         >Enter your login and password</InfoMessage>
         <Input className='login__input' placeholder='login' />
         <PasswordInput className='login__input' />
-      </div>
+        <Button
+          className='login__submit-button'
+          type='submit'
+          onClick={this.auth}
+        >Log In</Button>
+      </form>
     );
   }
 }
