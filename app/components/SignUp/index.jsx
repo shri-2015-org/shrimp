9e2346a9daffafa626e84807092d246f4077b0ce
Максим Самойлов <samoilowmaxim@gmail.com>
2015-09-27 23:20:40 +0300

    fix(input): fix passwordInput classes. Customize placeholder

--- app/components/SignUp/index.jsx
+++ app/components/SignUp/index.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import InfoMessage from 'components/InfoMessage';
+import PasswordInput from 'components/PasswordInput';
+import Input from 'components/Input';
+import Button from 'components/Button';
 
 import './styles.scss';
 
@@ -7,7 +11,19 @@ export default class SignUp extends React.Component {
 
   render() {
     return (
-      <div>SignUp</div>
+      <form className='sign-up'>
+        <InfoMessage
+          className='sign-up__info-message'
+        >Enter login and password</InfoMessage>
+        <Input className='sign-up__input' placeholder='Login' />
+        <PasswordInput className='sign-up__input' />
+        <PasswordInput className='sign-up__input' placeholder='Repeat password' />
+        <Button
+          className='sign-up__submit-button button_type_green'
+          type='submit'
+          onClick={this.auth}
+        >Sign Up</Button>
+      </form>
     );
   }
 }
