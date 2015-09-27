c6b11ecec1259da00e74e7222fc121e308452b6d
Максим Самойлов <samoilowmaxim@gmail.com>
2015-09-27 15:00:57 +0300

    feat(login-window): add tabs

--- app/components/LoginWindow/index.jsx
+++ app/components/LoginWindow/index.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
 import {Spring} from 'react-motion';
+import {List, Map} from 'immutable';
+import Tabs from 'components/Tabs';
 import './styles.scss';
 
 
 export default class LoginWindow extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTabId: 1,
+    };
+  }
+
+
+  changeTab = (tabId) => {
+    this.setState({
+      currentTabId: tabId,
+    });
+  }
+
+
   render() {
+    const tabs = List.of(
+      Map({id: 1, name: 'Log In'}),
+      Map({id: 2, name: 'Sign Up'}),
+    );
+
     const getLoginWindow = (interpolated) => (
       <div
         className='login-window'
         style={{transform: `scale(${interpolated.val})`}}
-      />
+      >
+        <Tabs
+          className='login-window__tabs'
+          tabClassName='login-window__tab'
+          tabs={tabs}
+          currentTabId={this.state.currentTabId}
+          changeTab={this.changeTab}
+        />
+      </div>
     );
 
     return (
