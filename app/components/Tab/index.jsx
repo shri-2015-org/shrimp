b31795501847f184756b022c398be6b4fdbc19e6
Максим Самойлов <samoilowmaxim@gmail.com>
2015-09-27 19:27:51 +0300

    feat(tabs): add link prop

--- app/components/Tab/index.jsx
+++ app/components/Tab/index.jsx
@@ -1,33 +1,41 @@
 import React, {PropTypes} from 'react';
+import {Link} from 'react-router';
 import cx from 'classnames';
 import './styles.scss';
 
+
 export default class Tab extends React.Component {
+
   static propTypes = {
     id: PropTypes.number.isRequired,
     isCurrent: PropTypes.bool,
-    name: PropTypes.string,
     changeTab: PropTypes.func,
     width: PropTypes.string,
     className: PropTypes.string,
+    link: PropTypes.string,
+    children: PropTypes.node,
   }
+
 
   constructor(props) {
     super(props);
   }
 
+
   shouldComponentUpdate(nextProps) {
     return nextProps.isCurrent !== this.props.isCurrent;
   }
+
 
   changeTab = (e) => {
     e.preventDefault();
     this.props.changeTab(this.props.id);
   }
 
+
   render() {
     const {className, isCurrent, width} = this.props;
-    return (
+    const tab = (
       <div
         className={cx('tabs__tab', {
           'tabs__tab_active': isCurrent,
@@ -36,8 +44,13 @@ export default class Tab extends React.Component {
         onClick={this.changeTab}
         style={{width: width}}
       >
-        {this.props.name}
+        {this.props.children}
       </div>
     );
+
+    if (this.props.link) {
+      return <Link to={this.props.link}>{tab}</Link>;
+    }
+    return tab;
   }
 }
