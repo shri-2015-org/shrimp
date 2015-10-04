5c1806750af471b27ed3e9c08c850e8e51ad4e60
Roman Fedorenkov <suffering123494@yandex.ru>
2015-10-04 12:20:37 +0300

    fix(auth): correct socket.io authorization for logged user

--- server/socket.js
+++ server/socket.js
@@ -1,9 +1,10 @@
 import Server from 'socket.io';
+import cookieParser from 'socket.io-cookie';
 import getInitState from './initial-state';
 import getMessageModel from './models/message';
 // import getUserModel from './models/user';
 import {SC, CS} from '../constants';
-import {signInUser, setSessionId} from './db/db_core.js';
+import {signInUser, setSessionId, checkUserSession} from './db/db_core.js';
 import {generateSessionId} from './lib/core.js';
 // const debug = require('debug')('shrimp:server');
 const Message = getMessageModel();
@@ -11,6 +12,22 @@ const Message = getMessageModel();
 
 export default function startSocketServer(http) {
   const io = new Server(http);
+
+  io.use(cookieParser);
+
+  io.set('authorization', function(handshakeData, callback) {
+    if (handshakeData.headers.cookie) {
+      const sessionId = handshakeData.headers.cookie.sessionId;
+
+      checkUserSession(sessionId).then(() => {
+        console.log('Success');
+        callback(null, true);
+      }).catch((exception) => {
+        console.log(exception);
+        callback(null, false);
+      });
+    }
+  });
 
   io.on('connection', socket => {
     socket.on(CS.INIT, data => {
