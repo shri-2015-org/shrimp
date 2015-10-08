12c65d8fa3ce2cea412099907f02641882bf8a02
Roman Fedorenkov <suffering123494@yandex.ru>
2015-10-08 09:17:33 +0300

    fix(redirect): move redirect checking in App component

--- app/Root.jsx
+++ app/Root.jsx
@@ -9,14 +9,6 @@ import cookies from 'browser-cookies';
 export default class Root extends React.Component {
 
   render() {
-    debugger;
-    const cookieSessionId = cookies.get('sessionId');
-    if (!cookieSessionId) {
-      if (location.pathname !== '/login' && location.pathname !== '/signup') {
-        store.history.pushState(null, '/login');
-      }
-    }
-
     const devTools = (() => {
       if (OPTIMIZED) {
         return null;
@@ -27,7 +19,7 @@ export default class Root extends React.Component {
           <DevTools
             store={store}
             monitor={LogMonitor}
-            visibleOnLoad={document.cookie.indexOf('enableDevTools=true') !== -1}
+            visibleOnLoad={cookies.get('enableDevTools')}
           />
         </DebugPanel>
       );
