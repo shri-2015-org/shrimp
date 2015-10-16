6e96e9aff51b655e0f692b24453bb9b7c0f46d6d
Faraser <faraser@rambler.ru>
2015-10-16 16:49:15 +0300

    Added client validation for creating channel

--- app/components/NewChannelItem/index.jsx
+++ app/components/NewChannelItem/index.jsx
@@ -1,6 +1,7 @@
 import React, {PropTypes} from 'react';
 import {Map, List} from 'immutable';
 import './styles.scss';
+import InfoMessage from 'components/InfoMessage';
 
 export default class ChannelItem extends React.Component {
 
@@ -13,8 +14,11 @@ export default class ChannelItem extends React.Component {
 
   constructor(props) {
     super(props);
+    this.channelNameMaxLength = 15;
     this.state = {
       channelName: '',
+      validationError: 'Add new channel',
+      infoType: null,
     };
   }
 
@@ -25,18 +29,17 @@ export default class ChannelItem extends React.Component {
   }
 
   onKeyPress = (e) => {
-    if (e.charCode !== 13) {
-      return;
+    if (e.which === 13) {
+      this.addChannel();
     }
-    this.addChannel();
   }
 
 
   addChannel = () => {
     const channelName = this.state.channelName.trim();
-    const sameChannel = this.props.channels.find(channel => channel.get('name') === channelName);
-    if (channelName && !sameChannel) {
-      this.props.replaceDirtyChannel(new Map({name: this.state.channelName, isDirty: true}));
+    const hasError = this.validateChannelName(channelName);
+    if (!hasError) {
+      this.props.replaceDirtyChannel(new Map({name: channelName, isDirty: true}));
       this.props.newChannel({name: channelName});
       const newChannelItem = this.refs.newChannel;
       newChannelItem.className = newChannelItem.className + ' new-channel_disabled';
@@ -44,13 +47,48 @@ export default class ChannelItem extends React.Component {
   }
 
   handleChange = (e) => {
+    this.validateChannelName(e.target.value);
     this.setState({channelName: e.target.value});
+  }
+
+  validateChannelName = (name) => {
+    const channelName = name.trim();
+    const sameChannel = this.props.channels.find(channel => channel.get('name') === channelName);
+    if (channelName.length > this.channelNameMaxLength) {
+      this.setState({
+        validationError: 'Name is very long',
+        infoType: 'error',
+      });
+      return true;
+    }
+    if (channelName.length === 0) {
+      this.setState({
+        validationError: 'Require channel name',
+        infoType: 'error',
+      });
+      return true;
+    }
+    if (sameChannel) {
+      this.setState({
+        validationError: 'Channel already exist',
+        infoType: 'error',
+      });
+      return true;
+    }
+    this.setState({
+      validationError: 'Add new channel',
+      infoType: null,
+    });
+    return false;
   }
 
   render() {
     return (
       <div className='new-channel' ref='newChannel'>
-        <p className='new-channel__title'>New channel name</p>
+        <InfoMessage
+          className='new-channel__info_message'
+          type={this.state.infoType}
+        ><span>{this.state.validationError}</span></InfoMessage>
         <input type='text'
           className='new-channel__input'
           ref='newChannelInput'
