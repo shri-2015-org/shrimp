23c7ce61ebf5c828108fdd1d0fe953bc382f698d
Максим Самойлов <samoilowmaxim@gmail.com>
2015-10-24 17:28:55 +0300

    feat(sokets): add validation

--- server/socket.js
+++ server/socket.js
@@ -1,5 +1,6 @@
 import Server from 'socket.io';
 import cookieParser from 'socket.io-cookie';
+import validate from 'valuedate';
 import getInitState from './initial-state';
 import getMessageModel from './models/message';
 import getChannelModel from './models/channel';
@@ -50,6 +51,7 @@ export default function startSocketServer(http) {
 
 
     socket.on(CS.JOIN_TO_CHANNEL, channelId => {
+      if (!validate(channelId).isString().inRange(20, 30).end()) return;
       joinToChannel(socket.sessionId, channelId, (userId) => {
         socket.join(channelId);
         socket.emit(SC.JOIN_TO_CHANNEL, {channelId, userId});
@@ -63,40 +65,56 @@ export default function startSocketServer(http) {
     });
 
 
-    socket.on(CS.ADD_MESSAGE, data => {
-      Message.add(data, (err, result) => {
-        io.to(data.channelId).emit(SC.ADD_MESSAGE, result.toObject());
+    socket.on(CS.ADD_MESSAGE, ({senderId, channelId, text}) => {
+      if (!(
+        validate(senderId).isString().inRange(20, 30).end() &&
+        validate(channelId).isString().inRange(20, 30).end() &&
+        validate(text).isString().inRange(0, 1000).end()
+      )) return;
+      Message.add({ senderId, channelId, text }, (err, result) => {
+        io.to(channelId).emit(SC.ADD_MESSAGE, result.toObject());
       });
     });
 
 
-    socket.on(CS.ADD_CHANNEL, data => {
-      Channel.add(data, (err, result) =>
+    socket.on(CS.ADD_CHANNEL, ({name}) => {
+      if (!(
+        validate(name).isString().inRange(1, 30).end()
+      )) return;
+      Channel.add({name}, (err, result) =>
         io.sockets.emit(SC.ADD_CHANNEL, result.toObject()));
     });
 
 
-    socket.on(CS.TYPING, data => {
-      io.socket.emit(SC.TYPING, {channelId: data.id, typing: true});
+    socket.on(CS.TYPING, ({id}) => {
+      if (!validate(id).isString().inRange(20, 30).end()) return;
+      io.socket.emit(SC.TYPING, {channelId: id, typing: true});
     });
 
 
-    socket.on(CS.SET_FAVORITE_CHANNEL, data => {
-      setFavoriteChannel(socket.sessionId, data);
+    socket.on(CS.SET_FAVORITE_CHANNEL, ({id}) => {
+      if (!validate(id).isString().inRange(20, 30).end()) return;
+      setFavoriteChannel(socket.sessionId, {id});
     });
 
 
-    socket.on(CS.CHANGE_USER_INFO, data => {
-      setUserInfo(socket.sessionId, data.email, data.name, data.language, (userData) => {
+    socket.on(CS.CHANGE_USER_INFO, ({email, name, language}) => {
+      if (!(
+        validate(email).isString().isEmail().inRange(1, 50).end() &&
+        validate(name).isString().inRange(1, 100).end() &&
+        validate(language).isString().inRange(1, 10).end()
+      )) return;
+      setUserInfo(socket.sessionId, email, name, (userData) => {
         socket.emit(SC.CHANGE_USER_INFO, {user: userData});
       });
     });
 
 
-    socket.on(CS.MARK_AS_READ, data => {
+    socket.on(CS.MARK_AS_READ, ({channelId}) => {
+      if (!validate(channelId).isString().inRange(20, 30).end()) return;
       User.getBySessionId(socket.sessionId)
       .then((user) => {
-        Channel.markAsRead(data, user.id);
+        Channel.markAsRead({channelId}, user.id);
       });
     });
 
@@ -118,10 +136,16 @@ export default function startSocketServer(http) {
       return res;
     }
 
-    socket.on(CS.ADD_DIRECT_CHANNEL, data => {
-      Channel.addDirectChannel(data, (err, result) => {
+    socket.on(CS.ADD_DIRECT_CHANNEL, ({name, userIds}) => {
+      if (!(
+        validate(name).isString().isEmail().inRange(1, 50).end() &&
+        userIds.length === 2 &&
+        userIds.reduce((p, n) => p && validate(n).isString().inRange(1, 50).end(), true)
+      )) return;
+
+      Channel.addDirectChannel({name, userIds}, (err, result) => {
         const channelId = result._id;
-        Promise.all(data.userIds.map(uid => User.getById(uid)))
+        Promise.all(({name, userIds}).userIds.map(uid => User.getById(uid)))
           .then(users => {
             const sessionIds = new Set(users.map(u => u.sessionId));
             const clients = findClientsSocket();
